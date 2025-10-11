3b31a9003840e82fa17df2e75fbaa3460a8320ab
Kenny <kennythedev01@gmail.com>
2025-10-11 14:42:53 +0100

    Did the movie section today. Lets goo

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
      'September', 'October', 'November', 'December'];
 const main = document.getElementById('hii');
 const loginSec = document.getElementById('login-section');
+const formSec = document.getElementById('form-section');
+const msg = document.getElementById('message');
+const userName = document.getElementById('username');
+const movieSec = document.querySelector('.movie-section')
 
+//Smooth animation effect for home page
 main.addEventListener('click', () =>{
     main.style.transition = 'opacity 0.6s';
     main.style.opacity = '0';
@@ -21,6 +26,25 @@ main.addEventListener('click', () =>{
     }, 200);
 });
 
+//Authentication time!!
+formSec.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    const usernameValue = userName.value.trim();
+    if (usernameValue == 'admin'){
+        msg.textContent = 'Correct✅';
+        msg.style.transition = 'opacity 2s';
+        msg.style.opacity = '0';
+        setTimeout(()=>{
+            msg.textContent = '';
+            loginSec.style.display = 'none';
+            movieSec.style.display = 'flex';
+        }, 2000)
+        
+    }
+    else{
+        msg.textContent = 'holup';
+    }
+})
 
 
 //Check tis out!!
