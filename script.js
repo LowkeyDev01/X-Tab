206b21dfa3a27f4f0a021fa6e70fdd61eb990bc7
Kenny <kananaya335@gmail.com>
2025-12-08 23:13:30 +0100

    made the Ui for the login section, about to do authentication

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 //Declaring my variables
 const homeSec = document.getElementById('home');
 const loginSec = document.getElementById('login');
-const loginBtn = document.getElementById('loginbtn')
+const signUpSec = document.getElementById('signup');
+const loginBtn = document.querySelector('#loginbtn')
+const signupBtn = document.querySelector('#signupbtn')
+const check = document.querySelector('#agree')
 
-function swap(currentPage, newPage){
-    currentPage.style.display = 'none';
-    newPage.style.display = 'flex'
+//swap function(to avoid repeating)
+function swap(button, currentPage, newPage) {
+    button.addEventListener('click', () => {
+            currentPage.style.transition = '0.9s opacity'
+            currentPage.style.opacity = 0
+        setTimeout(() => {
+            currentPage.style.display = 'none';
+            newPage.style.display = 'flex'
+        }, 900)
+    })
 }
-loginBtn.addEventListener('click', ()=>{
-    swap(homeSec, loginSec)
-})
+
+//all the swaps needed
+swap(loginBtn, homeSec, loginSec);
+swap(signupBtn, homeSec, signUpSec);
