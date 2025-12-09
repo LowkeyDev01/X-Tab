523a8e50116c40fbe55a3c8a1e8bdbd973f25c7f
Kenny <kananaya335@gmail.com>
2025-12-09 14:28:08 +0100

    check it

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const homeSec = document.getElementById('home');
 const loginSec = document.getElementById('login');
 const signUpSec = document.getElementById('signup');
+const main = document.getElementById('main');
 const loginBtn = document.querySelector('#loginbtn')
 const signupBtn = document.querySelector('#signupbtn')
 const check = document.querySelector('#agree')
@@ -9,8 +10,8 @@ const check = document.querySelector('#agree')
 //swap function(to avoid repeating)
 function swap(button, currentPage, newPage) {
     button.addEventListener('click', () => {
-            currentPage.style.transition = '0.9s opacity'
-            currentPage.style.opacity = 0
+        currentPage.style.transition = '0.9s opacity'
+        currentPage.style.opacity = 0
         setTimeout(() => {
             currentPage.style.display = 'none';
             newPage.style.display = 'flex'
@@ -21,3 +22,26 @@ function swap(button, currentPage, newPage) {
 //all the swaps needed
 swap(loginBtn, homeSec, loginSec);
 swap(signupBtn, homeSec, signUpSec);
+
+
+//tiny Auth
+const userName = document.getElementById('username')
+const password = document.getElementById('password')
+const log = document.getElementById('ggs')
+const err = document.getElementById('error')
+log.addEventListener('click', () => {
+    if (userName.value == 'admin' && password.value == 'admin') {
+        loginSec.style.transition = '0.9s opacity'
+        loginSec.style.opacity = 0
+        setTimeout(() => {
+            loginSec.style.display = 'none';
+            main.style.display = 'flex'
+            err.style.display = 'none'
+            err.textContent = ''
+        }, 900)
+    }
+    else {
+        err.style.display = 'block'
+        err.textContent = 'try again'
+    }
+})
