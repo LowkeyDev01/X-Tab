55f86524c6ad92d7e16593fef3718531d4881937
Kenny <kananaya335@gmail.com>
2025-12-10 21:44:23 +0100

    add scroll effect

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //Declaring my variables
+const nav = document.getElementById('nav')
+const topet = document.getElementById('top')
+
+let lastScroll = 0
+window.addEventListener('scroll', ()=>{
+    currentScroll = window.scrollY
+
+    if (currentScroll > lastScroll){
+        nav.style.bottom = '-80px'
+    }
+    else{
+        nav.style.bottom = '0px'
+    }
+    lastScroll = currentScroll
+})
