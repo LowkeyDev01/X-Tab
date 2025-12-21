044314a53e0e4711158dea63d7e58d4d74f34d85
Kenny <kananaya335@gmail.com>
2025-12-21 12:44:54 +0100

    hore

--- script.js
+++ script.js
@@ -3,14 +3,33 @@ const nav = document.getElementById('nav')
 const topet = document.getElementById('top')
 
 let lastScroll = 0
-window.addEventListener('scroll', ()=>{
+window.addEventListener('scroll', () => {
     currentScroll = window.scrollY
 
-    if (currentScroll > lastScroll){
+    if (currentScroll > lastScroll) {
         nav.style.bottom = '-80px'
     }
-    else{
+    else {
         nav.style.bottom = '0px'
     }
     lastScroll = currentScroll
 })
+
+function swap() {
+    const navLinks = document.querySelectorAll('.w')
+    const pages = document.querySelectorAll('.page')
+
+    navLinks.forEach(btn => {
+        btn.addEventListener('click', () => {
+            targetId = btn.dataset.target;
+            navLinks.forEach(b => b.classList.remove('q'))
+
+            btn.classList.add('q')
+
+            pages.forEach(p => p.style.display = 'none')
+            document.getElementById(targetId).style.display = 'flex'
+        })
+    })
+}
+swap()
+
