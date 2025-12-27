1806e3c6ac298d4b21332606f7ce3422d4f76174
Kenny <kananaya335@gmail.com>
2025-12-27 15:06:39 +0100

    ss

--- script.js
+++ script.js
@@ -2,19 +2,6 @@
 const nav = document.getElementById('nav')
 const topet = document.getElementById('top')
 
-let lastScroll = 0
-window.addEventListener('scroll', () => {
-    currentScroll = window.scrollY
-
-    if (currentScroll > lastScroll) {
-        nav.style.bottom = '-80px'
-    }
-    else {
-        nav.style.bottom = '0px'
-    }
-    lastScroll = currentScroll
-})
-
 function swap() {
     const navLinks = document.querySelectorAll('.w')
     const pages = document.querySelectorAll('.page')
